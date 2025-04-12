3b8674f10f97c0000c4dfafb92ded4c1d7672596
Hasan Böcek <bocekhasan@icloud.com>
2025-04-12 15:11:41 +0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,7 +36,7 @@
           </div>
         </div>
         <div class="lg:w-1/2 mt-10 lg:mt-0 flex justify-center">
-          <img src="https://i.imgur.com/5jB8GVw.jpeg" alt="Hasan BÖCEK" class="w-64 h-64 md:w-80 md:h-80 rounded-full shadow-xl object-cover">
+          <img src="https://i.imgur.com/zeFmsF1.jpeg" alt="Hasan BÖCEK" class="w-64 h-64 md:w-80 md:h-80 rounded-full shadow-xl object-cover">
         </div>
       </div>
     </div>
